4c408fe0fdf08ac34758543e969319f90a06526a
Hsonu <sonurajsonuraj4515@gmail.com>
2026-02-26 19:12:43 +0530

    js

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("Welcome to Javascript");
+
+//Variable Declaration
+let x = 10;
+var y = 20;
+const z = 40;
+
+
+//printing some arithmatic operation
+console.log(x+y+z); // 70
+console.log(x-y-z);
+console.log(x*y*z);
+console.log(x*y*z);
+console.log(x/y/z);
+
+//printing increment and decrement  prefix
+
+
+y = ++y;
+
+console.log(y);
+
+let a = 1;
+let b = --a; 
+
+console.log(b); // output 0
+
+
+//printing increment and decrement  postfix 
+let g = 1;
+let h = g++; 
+
+
+console.log(h);
+console.log(g);
